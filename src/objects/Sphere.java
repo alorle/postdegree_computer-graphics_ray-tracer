9a1668b01613b2d2e0df7974056ee46cc51c7438
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2017-11-07 18:45:22 +0100

    objects(sphere): Implement intersection with rays outside the sphere (#4)
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- src/objects/Sphere.java
+++ src/objects/Sphere.java
@@ -26,9 +26,58 @@ public class Sphere extends Object3D {
 
   @Override
   protected Hit _intersects(final Ray ray) {
+    final float r = this.radio;
+    final Point3D C = this.centro;
+    final Point3D R = ray.getStartingPoint();
+    final Vector3D v = ray.getDirection();
 
-    return Hit.NOHIT;
+    final Vector3D RC = new Vector3D(R, C);
 
+    final float c = RC.dot(RC) - r * r;
+    if (c > 0) {
+      // Punto origen del rayo, R, fuera de la esfera
+      final float b = RC.dot(v);
+      final float bSquare = b * b;
+
+      if (b > 0) {
+        // Centro C de la esfera en semiespacio posterior, puede existir
+        // intersección
+        final float d = (float) Math.sqrt(bSquare - c);
+        final float dSign = Math.signum(d);
+
+        if (dSign == 0) {
+          // Rayo tangente a la esfera
+          float t = b;
+          final Point3D P = ray.pointAtParameter(t);
+          final Vector3D normal = new Vector3D(C, P);
+          normal.normalize();
+
+          return new Hit(t, P, normal, this);
+        } else if (dSign > 0) {
+          // Rayo secante a la esfera
+          final float tp = b + d;
+          final float tm = c / tp;
+
+          final Point3D P = ray.pointAtParameter(tm);
+          final Vector3D normal = new Vector3D(C, P);
+          normal.normalize();
+
+          return new Hit(tm, P, normal, this);
+        }
+
+        // Rayo no corta a la esfera
+        return Hit.NOHIT;
+      }
+
+      // Centro C de la esfera en semiespacio anterior
+      return Hit.NOHIT;
+    } else if (c < 0) {
+      // Punto de origen dentro de la esfera
+      throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    // Punto de origen en la superficie de la esfera
+    return new Hit(0, R, RC, this);
   }
 
 }
