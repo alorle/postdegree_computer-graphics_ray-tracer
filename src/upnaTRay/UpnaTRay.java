7cac35c9ecc48ca80f82720afeef17f53681d731
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2017-11-21 17:22:52 +0100

    If no params, get input file name from user input
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- src/upnaTRay/UpnaTRay.java
+++ src/upnaTRay/UpnaTRay.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 
 import view.Camera;
 import gui.Image;
+import java.util.Scanner;
 import objects.Group3D;
 import parser.Parser;
 
@@ -22,12 +23,24 @@ public final class UpnaTRay {
 
     final String path = System.getProperty("user.dir") + File.separator
             + "scenes" + File.separator;
-    final File inputFile = new File(path + args[0]);
+
+    String inputFileName;
+    if (args.length < 1) {
+      final Scanner input = new Scanner(System.in);
+      System.out.print("Input file name?: ");
+      inputFileName = input.nextLine() + ".xml";
+    } else {
+      inputFileName = args[0];
+    }
+
+    final File inputFile = new File(path + inputFileName);
     final Parser parser = new Parser(inputFile);
 
     final Image image = parser.getViewport();
     final Camera camera = parser.getCamera();
     final Group3D scene = parser.getScene();
+
+    System.out.println("Rendering " + inputFile.getName());
 
     final long t0 = System.nanoTime();
     image.synthesis(camera, scene);
